33247e624ddd28e633fdbce51e497ef6c75c9957
adam.raczynski <raq000@gmail.com>
2018-12-13 23:09:47 +0100

    Initial commit - circling done.

---
+++ FourierCircles/Harmonic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FourierCircles
+{
+    public class Harmonic : INotifyPropertyChanged
+    {
+        public double Length { get; set; }
+        public double Phase { get; set; }
+        public Point Origin { get; set; }
+        public Point End => new Point(Origin.X + Math.Sin(Phase) * Length, Origin.Y + Math.Cos(Phase) * Length);
+        public double Freq { get; set; }
+        public Harmonic SubHarmonic { get; private set; }
+        public Harmonic ParentHarmonic { get; private set; }
+        public Harmonic(double length, double phase, double freq)
+        {
+            Length = length;
+            Phase = phase;
+            Freq = freq;
+            Origin = new Point(0, 0);
+        }
+        public Harmonic AddHarmonic(double length, double phase, double freq)
+        {
+            SubHarmonic = new Harmonic(length, phase, freq);
+            SubHarmonic.ParentHarmonic = this;
+            SubHarmonic.Origin = End;
+            return SubHarmonic;
+        }
+        public IEnumerable<Harmonic> ListHarmonics()
+        {
+            var list = new List<Harmonic>();
+            list.Add(this);
+            if (SubHarmonic != null) list.AddRange(SubHarmonic.ListHarmonics());
+            return list;
+        }
+        public void Tick()
+        {
+            Phase += Freq;
+            Origin = ParentHarmonic?.End ?? Origin;
+            OnPropertyChanged("Origin");
+            OnPropertyChanged("End");
+            SubHarmonic?.Tick();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ FourierCircles/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace FourierCircles
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Harmonic> Harmonics { get; set; }
+        public Harmonic Last { get; set; }
+        private Harmonic fourier;
+        public MainViewModel()
+        {
+            fourier = new Harmonic(100, 30, 0.01);
+            fourier.AddHarmonic(40, -30, 0.04)
+                .AddHarmonic(20,0,0.08);
+            Harmonics = new ObservableCollection<Harmonic>(fourier.ListHarmonics());
+            Last = Harmonics.Last();
+        }
+        public void Run()
+        {
+            var dt = new DispatcherTimer();
+            dt.Tick += (s, e) => fourier.Tick();
+            dt.Interval = TimeSpan.FromMilliseconds(20);
+            dt.Start();
+        }
+    }
+}
