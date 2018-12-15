e2007ca6f3216d02ec91221077615f6030e77fec
Adam Raczynski <raq000@gmail.com>
2018-12-15 23:54:54 +0100

    test proj + pipe shell

---
+++ FourierCircles/Pipe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourierCircles
+{
+    public class Pipe<T> : IEnumerable<T>
+    {
+        List<T> pipeList;
+        private readonly int capacity;
+        public Pipe(int capacity)
+        {
+            this.capacity = capacity >= 0 ? capacity : 0;
+            pipeList = new List<T>(capacity);
+        }
+        public void Add(T item)
+        {
+
+        }
+        public void AddRange(IEnumerable<T> items)
+        {
+
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FourierCirclesTests/PipeTests.cs
@@ -0,0 +1,27 @@
+﻿using FourierCircles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FourierCirclesTests
+{
+    public class PipeTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(-1)]
+        public void SetCapacity(int capacity)
+        {
+            var pipe = new Pipe<int>(capacity);
+            pipe.AddRange(Enumerable.Range(0,capacity + 1));
+            Assert.Equal(capacity, pipe.Count());
+
+        }
+    }
+}
