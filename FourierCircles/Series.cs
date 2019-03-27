dc81493fce12253816ca3ad7403ef7ea5654bac6
Adam Raczynski <raq000@gmail.com>
2019-03-27 19:58:22 +0100

    bash test

---
+++ FourierCircles/Series.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourierCircles
+{
+    public class Series
+    {
+        private readonly List<Harmonic> _harmonics;
+        public IEnumerable<Harmonic> Harmonics => _harmonics;
+        public Series()
+        {
+            _harmonics = new List<Harmonic>();
+        }
+
+        public Harmonic AddHarmonic(double amplify, double phase, double freq)
+        {
+            var harmonic = new Harmonic(amplify, phase, freq);
+            _harmonics.Add(harmonic);
+            return harmonic;
+        }
+    }
+}
--- FourierCirclesTests/SeriesTests.cs
+++ FourierCirclesTests/SeriesTests.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using FourierCircles;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace FourierCirclesTests
 {
     public class SeriesTests
     {
+        [Fact]
+        public void Ctor()
+        {
+            var roi = new Series();
+            var harmonic = roi.AddHarmonic(1, 0, 1);
+            Assert.Contains(harmonic,roi.Harmonics);
+        }
     }
 }
